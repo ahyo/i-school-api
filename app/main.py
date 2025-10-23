b6bdf5e94318904483c3907d335e90579138d9bd
Ahyo Haryanto <ahyo@Mac.local>
2025-10-23 09:27:15 +0700

    update

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.api import api_router
 from app.core.config import settings
 
 
-# aneh ya a
+# aneh ya aa
 def create_app() -> FastAPI:
     app = FastAPI(
         title=settings.app_nama,
